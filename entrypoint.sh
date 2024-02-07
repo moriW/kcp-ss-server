b17f8038da761cc50108b21edaf64372a06ffeb1
yuhua.zhao <yuhua.zhao@happyelments.com>
2024-02-07 10:23:21 +0800

    update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,5 @@ ss-server -s :: -s 0.0.0.0 -p 8338 -k "${SS_PASSWORD}" -m ${SS_METHOD} -u &
 	--crypt none \
 	--mode fast3 \
 	--ds 10 \
-	--ps 3 \
-	--sockbuf 16777217
+	--ps 3
 # FROM https://github.com/xtaci/kcptun
