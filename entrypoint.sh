9148787300f18c7e10ffffd26da86cecc59abffa
mori <moridevelopers@gmail.com>
2024-02-25 11:17:05 +0800

    fix try

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ ss-server -s :: -s 0.0.0.0 -p 8338 -k "${SS_PASSWORD}" -m ${SS_METHOD} -u &
 	--crypt none \
 	--mode fast3 \
 	--ds 10 \
-	--ps 3
+	--ps 3 \
+	--rcvwnd 1024 \
+	--sndwnd 1024 \
+	--mtu 1350
+
 # FROM https://github.com/xtaci/kcptun
